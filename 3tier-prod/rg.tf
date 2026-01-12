533a4405728b5a829cea12da005226b2c417001c
SOMNATH VERMA <somverma.earth@outlook.com>
2026-01-12 03:29:02 -0500

    added rgsom3

--- 3tier-prod/rg.tf
+++ 3tier-prod/rg.tf
@@ -12,3 +12,8 @@ resource "azurerm_resource_group" "rgsom2" {
   name     = var.rg_name
   location = var.location
 }
+
+resource "azurerm_resource_group" "rgsom3" {
+  name     = var.rg_name
+  location = var.location
+}
