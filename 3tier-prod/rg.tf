cec384a8d18652ca6dfd3bb8a0dbb8ed5b34c1c8
SOMNATH VERMA <somverma.earth@outlook.com>
2026-01-12 03:03:55 -0500

    added rgsom1

---
+++ 3tier-prod/rg.tf
@@ -0,0 +1,9 @@
+resource "azurerm_resource_group" "rgsom1" {
+  name     = var.rg_name
+  location = var.location
+}
+
+resource "azurerm_resource_group" "rgsom1" {
+  name     = var.rg_name
+  location = var.location
+}
